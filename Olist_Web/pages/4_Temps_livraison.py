66baec2a77647592a60c2324897ee61841d9f4d2
LaGuize <guilhem.dolet@gmail.com>
2024-02-02 11:17:48 +0100

    V2

--- Olist_Web/pages/4_Temps_livraison.py
+++ Olist_Web/pages/4_Temps_livraison.py
@@ -2,3 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+livraison_df = pd.read_csv("DataSet/temps_livraison.csv")
+
+histo_livraison = px.histogram((livraison_df), x="temps_livraison", range_x=[0, 50])
+st.plotly_chart(histo_livraison)
+
